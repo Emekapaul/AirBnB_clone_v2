8558818b26904fd4625266ccd5b82893c3d55916
Paul Nnaemeka <emekapaul302@gmail.com>
2024-04-21 05:55:34 +0100

    Solve task 4 web_flask

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application:"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays Hello HBNB! in browser"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays HBNB in browser"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def dynamic_text(text):
+    """Displays Dynamic text in browser"""
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def dynamic_text_default(text="is cool"):
+    """Displays Dynamic text in browser"""
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def display_integer(n):
+    """Displays Integer in browser"""
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
